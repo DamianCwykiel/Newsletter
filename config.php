7443a45d4edc1488b8d7572912e94ffbfd938118
DamianCwykiel <damian.cwykiel@gmail.com>
2019-02-02 21:25:18 +0100

    files modified

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?
 	return[
-		'host' =>'shareddb-g.hosting.stackcp.net',
-		'user' =>'newsletter-3235fbbb',
-		'password'=> 'skarbyislamu55',
-		'database' => 'newsletter-3235fbbb'
+		'host' =>'xxxxxx',
+		'user' =>'xxxxxx',
+		'password'=> 'xxxxx',
+		'database' => 'xxxxxxx'
 ];
 ?>
